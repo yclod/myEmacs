127ca4666f1a3e6b0d5c942364cef45493144e9f
Weizhi Yang <clod.yang@gmail.com>
2017-12-12 23:09:41 -0800

    tab tab tab..

--- init.el
+++ init.el
@@ -14,45 +14,46 @@
 
 (require 'cl)
 
- ;; Add Packages
- (defvar my/packages '(
-		;; --- Auto-completion ---
-		company
-		;; --- Better Editor ---
-		hungry-delete
-		swiper
-		counsel
-		smartparens
-		evil-nerd-commenter
-		popwin
-		fiplr
-		;; --- file tool ---
-		reveal-in-osx-finder
-		helm-ag
-		;; --- Major Mode ---
-		js2-mode
-		;; --- Minor Mode ---
-		nodejs-repl
-		exec-path-from-shell
-		) "Default packages")
+;; Add Packages
+(defvar my/packages '(
+		      ;; --- Auto-completion ---
+		      company
+		      ;; --- Better Editor ---
+		      hungry-delete
+		      swiper
+		      counsel
+		      smartparens
+		      evil-nerd-commenter
+		      popwin
+		      fiplr
+		      swift-mode
+		      ;; --- file tool ---
+		      reveal-in-osx-finder
+		      helm-ag
+		      ;; --- Major Mode ---
+		      js2-mode
+		      ;; --- Minor Mode ---
+		      nodejs-repl
+		      exec-path-from-shell
+		      ) "Default packages")
 
- (setq package-selected-packages my/packages)
+(setq package-selected-packages my/packages)
 
- (defun my/packages-installed-p ()
-     (loop for pkg in my/packages
-	   when (not (package-installed-p pkg)) do (return nil)
-	   finally (return t)))
+(defun my/packages-installed-p ()
+  (loop for pkg in my/packages
+	when (not (package-installed-p pkg)) do (return nil)
+	finally (return t)))
 
- (unless (my/packages-installed-p)
-     (message "%s" "Refreshing package database...")
-     (package-refresh-contents)
-     (dolist (pkg my/packages)
-       (when (not (package-installed-p pkg))
-	 (package-install pkg))))
+(unless (my/packages-installed-p)
+  (message "%s" "Refreshing package database...")
+  (package-refresh-contents)
+  (dolist (pkg my/packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg))))
 
- ;; Find Executable Path on OS X
- (when (memq window-system '(mac ns))
-   (exec-path-from-shell-initialize))
+;; Find Executable Path on OS X
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
 (tool-bar-mode -1)
 (if (display-graphic-p)
     (progn
@@ -62,6 +63,7 @@
 (global-company-mode 1)
 (setq cursor-type 'bar)
 (setq inhibit-splash-screen 1)
+(electric-indent-mode -1)
 (setq make-backup-file nil)
 (set-face-attribute 'default nil :height 160)
 (defun open-init-file()
@@ -94,7 +96,7 @@
 (smartparens-global-mode 1)
 (require 'hungry-delete)
 (global-hungry-delete-mode)
-;(setq initial-frame-alist (quote ((fullscreen . maximized))))
+					;(setq initial-frame-alist (quote ((fullscreen . maximized))))
 (evilnc-default-hotkeys)
 (global-set-key (kbd "M-;") 'evilnc-comment-or-uncomment-lines)
 (global-set-key (kbd "C-c l") 'evilnc-quick-comment-or-uncomment-to-the-line)
@@ -123,9 +125,9 @@
 (require 'popwin)
 (popwin-mode 1)
 ;; autolad octave mode for *.m-files
-       (autoload 'octave "octave-mod" nil t)
-       (setq auto-mode-alist
-	     (cons '("\\.m$" . octave-mode) auto-mode-alist))
+(autoload 'octave "octave-mod" nil t)
+(setq auto-mode-alist
+      (cons '("\\.m$" . octave-mode) auto-mode-alist))
 (add-hook 'octave-mode-hook
           (lambda ()
             (abbrev-mode 1)
@@ -137,7 +139,7 @@
 (global-set-key "\C-x\ \C-r" 'recentf-open-files)
 (setq fiplr-ignored-globs '((directories (".git" ".svn"))
                             (files ("*.jpg" "*.png" "*.zip" "*~"))))
-(global-set-key (kbd "C-x o") 'fiplr-find-file)
+(global-set-key (kbd "C-x f") 'fiplr-find-file)
 (global-auto-revert-mode 1)
 (fset 'yes-or-no-p 'y-or-n-p)
 
@@ -165,4 +167,117 @@
 	     (funcall fn)))))
 
 ;; ag search
-(global-set-key (kbd "C-x f") 'helm-do-ag-project-root)
+(global-set-key (kbd "C-x p") 'helm-do-ag-project-root)
+(defun indent-buffer()
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+(defun indent-region-or-buffer()
+  (interactive)
+  (save-excursion
+    (if (region-active-p)
+	(progn
+	  (indent-region (region-beginning) (region-end))
+	  (message "Indent selected region."))
+      (progn
+	(indent-buffer)
+	(message "Indent buffer.")))))
+(global-set-key (kbd "C-M-\\") 'indent-region-or-buffer)
+
+
+
+;; turn on transient mark mode
+;;(that is, we highlight the selected text)
+(transient-mark-mode t)
+
+(setq my-tab-width 4)
+
+(defun indent-block()
+  (shift-region my-tab-width)
+  (setq deactivate-mark nil))
+
+(defun unindent-block()
+  (shift-region (- my-tab-width))
+  (setq deactivate-mark nil))
+
+(defun shift-region(numcols)
+" my trick to expand the region to the beginning and end of the area selected
+ much in the handy way I liked in the Dreamweaver editor."
+  (if (< (point)(mark))
+    (if (not(bolp))    (progn (beginning-of-line)(exchange-point-and-mark) (end-of-line)))
+    (progn (end-of-line)(exchange-point-and-mark)(beginning-of-line)))
+  (setq region-start (region-beginning))
+  (setq region-finish (region-end))
+  (save-excursion
+    (if (< (point) (mark)) (exchange-point-and-mark))
+    (let ((save-mark (mark)))
+      (indent-rigidly region-start region-finish numcols))))
+
+(defun indent-or-complete ()
+  "Indent region selected as a block; if no selection present either indent according to mode,
+or expand the word preceding point. "
+  (interactive)
+  (if  mark-active
+      (indent-block)
+    (if (looking-at "\\>")
+  (hippie-expand nil)
+      (insert "\t"))))
+
+(defun my-unindent()
+  "Unindent line, or block if it's a region selected.
+When pressing Shift+tab, erase words backward (one at a time) up to the beginning of line.
+Now it correctly stops at the beginning of the line when the pointer is at the first char of an indented line. Before the command would (unconveniently)  kill all the white spaces, as well as the last word of the previous line."
+
+  (interactive)
+  (if mark-active
+      (unindent-block)
+    (progn
+      (unless(bolp)
+        (if (looking-back "^[ \t]*")
+            (progn
+              ;;"a" holds how many spaces are there to the beginning of the line
+              (let ((a (length(buffer-substring-no-properties (point-at-bol) (point)))))
+                (progn
+                  ;; delete backwards progressively in my-tab-width steps, but without going further of the beginning of line.
+                  (if (> a my-tab-width)
+                      (delete-backward-char my-tab-width)
+                    (backward-delete-char a)))))
+          ;; delete tab and spaces first, if at least 2 exist, before removing words
+          (progn
+            (if(looking-back "[ \t]\\{2,\\}")
+                (delete-horizontal-space)
+              (backward-kill-word 1))))))))
+
+(add-hook 'find-file-hooks (function (lambda ()
+ (unless (eq major-mode 'org-mode)
+(local-set-key (kbd "<tab>") 'indent-or-complete)))))
+
+(if (not (eq  major-mode 'org-mode))
+    (progn
+      (define-key global-map "\t" 'indent-or-complete) ;; with this you have to force tab (C-q-tab) to insert a tab after a word
+      (define-key global-map [S-tab] 'my-unindent)
+      (define-key global-map [C-S-tab] 'my-unindent)))
+
+;; mac and pc users would like selecting text this way
+(defun dave-shift-mouse-select (event)
+ "Set the mark and then move point to the position clicked on with
+ the mouse. This should be bound to a mouse click event type."
+ (interactive "e")
+ (mouse-minibuffer-check event)
+ (if mark-active (exchange-point-and-mark))
+ (set-mark-command nil)
+ ;; Use event-end in case called from mouse-drag-region.
+ ;; If EVENT is a click, event-end and event-start give same value.
+ (posn-set-point (event-end event)))
+
+;; be aware that this overrides the function for picking a font. you can still call the command
+;; directly from the minibufer doing: "M-x mouse-set-font"
+(define-key global-map [S-down-mouse-1] 'dave-shift-mouse-select)
+
+;; to use in into emacs for  unix I  needed this instead
+; define-key global-map [S-mouse-1] 'dave-shift-mouse-select)
+
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; this final line is only necessary to escape the *scratch* fundamental-mode
+;; and let this demonstration work
+(text-mode)
